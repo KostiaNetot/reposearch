e564c92859a8fffd1b34108b29a4e43f703114fa
Kostia Netot <pugjazz@gmail.com>
2019-12-12 17:52:51 +0200

    deleted spare comments

--- js/creatingElement.js
+++ js/creatingElement.js
@@ -1,19 +1,21 @@
-// 'use strict';
+'use strict';
 
-// const createElement = (options) => {
-//   let element = document.createElement(options.tagName);
-//   if ('className' in options) {
-//     element.setAttribute('class', options.className);
-//   }
-//   if ('innerText' in options) {
-//     element.innerText = options.innerText;
-//   }
-//   if ('dataName' in options) {
-//     element.setAttribute('data-name', options.dataName);
-//   }
-//   if ('event' in options) {
-//     element.addEventListener(options.event, options.handler);
-//   }
-//   options.parent.appendChild(element);
-//   return element;
-// };
+const insertItemCard = (obj) => {
+  let card = `
+    <div class="item-card" data-id="${obj.id}">
+    <div class="item-card-inner-wrap">
+      <div class="img-wrapper">
+        <img class="avatar" src="${obj.ava}" alt="avatar">
+      </div>
+      <div class="text-wrapper">
+        <h5 class="nameInfo">${obj.name}</h5>
+        <p>id: <span class="bold idInfo">#${obj.id}</span></p>
+        <p>owner: <a class="bold owner-info" href="${obj.owUrl}" target="_blank" title="owner page">${obj.owner}</a></p>
+        <p><a class="link link-info" target="_blank" href="${obj.url}" title="repos page">repository link...</a></p>
+      </div>
+    </div>
+  </div>
+`;
+  let itemCardwrapper = document.getElementById('itemsWrapper');
+  itemCardwrapper.innerHTML += card;
+};
--- js/functions.js
+++ js/functions.js
@@ -50,26 +50,6 @@ const setNewRepositoryCard = (id, name, owner, url, owUrl, ava) => {
   return newRepositoryCard;
 };
 
-const insertItemCard = (obj) => {
-  let card = `
-    <div class="item-card" data-id="${obj.id}">
-    <div class="item-card-inner-wrap">
-      <div class="img-wrapper">
-        <img class="avatar" src="${obj.ava}" alt="avatar">
-      </div>
-      <div class="text-wrapper">
-        <h5 class="nameInfo">${obj.name}</h5>
-        <p>id: <span class="bold idInfo">#${obj.id}</span></p>
-        <p>owner: <a class="bold owner-info" href="${obj.owUrl}" target="_blank" title="owner page">${obj.owner}</a></p>
-        <p><a class="link link-info" target="_blank" href="${obj.url}" title="repos page">repository link...</a></p>
-      </div>
-    </div>
-  </div>
-`;
-  let itemCardwrapper = document.getElementById('itemsWrapper');
-  itemCardwrapper.innerHTML += card;
-};
-
 const copyArr = (res, defArr) => {
   defArr.forEach((obj) => {
     res.push(obj);
